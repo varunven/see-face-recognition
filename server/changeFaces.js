c1e8676733d7dd0eaaf8c9586f5acd48bd0055ab
Varun Venkatesh <39748296+varunven@users.noreply.github.com>
2023-05-31 14:39:25 -0700

    Delete server directory
    deleting testing server files

--- server/changeFaces.js
+++
@@ -1,9 +0,0 @@
-// handle changing faces
-
-function handlechangeFaces(faceID, origName, newName) {
-  // Process input and return data
-  const result = { message: `${ faceID } has been renamed from ${ origName } to ${newName}` };
-  return result;
-}
-
-module.exports.handlechangeFaces = handlechangeFaces;
